c8ecc8425511bb736aeca6ebf29d461611cad11f
sasikumar2611 <sasikumar2611001@gmail.com>
2024-07-17 20:22:05 +0530

    commit

---
+++ wallclock.js
@@ -0,0 +1,23 @@
+const hourHand = document.getElementById('hour');
+const minuteHand = document.getElementById('minute');
+const secondHand = document.getElementById('second');
+
+function setClock() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setClock, 1000);
+
+setClock(); // Set the initial position of the clock hands
